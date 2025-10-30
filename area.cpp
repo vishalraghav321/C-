2b1d86a019fe5db5572e177b26182864bb228eee
vishalraghav321 <vishalraghav321@gmail.com>
2025-10-30 08:03:13 +0530

    formatting

--- area.cpp
+++ area.cpp
@@ -7,16 +7,15 @@ int main() {
 
     cout << "Enter radius of circle: ";
     cin >> radius;
-    areaCircle = 3.14159 * radius * radius;
-    cout << "Area of Circle = " << areaCircle << endl;
-
     cout << "Enter side of square: ";
     cin >> side;
-    areaSquare = side * side;
-    cout << "Area of Square = " << areaSquare << endl;
-
     cout << "Enter length and breadth of rectangle: ";
     cin >> length >> breadth;
+
+    areaCircle = 3.14 * radius * radius;
+    cout << "Area of Circle = " << areaCircle << endl;
+    areaSquare = side * side;
+    cout << "Area of Square = " << areaSquare << endl;
     areaRectangle = length * breadth;
     cout << "Area of Rectangle = " << areaRectangle << endl;
 
