23b6cbf7e455a3524075669b883acdd87cbf8210
Vishal Raghav <vishalraghav321@gmail.com>
2025-11-11 13:59:43 +0530

    vector

---
+++ vector.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q,l,j,a,k;
+    cin >> n >> q;
+    vector<vector<int>> vec(n);
+     for(int i =0; i<n; i++){
+        cin>>a;
+         for(int j =0; j<a; j++){
+            cin>>l;
+        vec[i].push_back(l);
+    }
+    }
+    for(int i =0; i<q; i++){
+        cin>>j>>k;
+        cout<<vec[j][k]<< endl;
+    }
+    return 0;
+}
