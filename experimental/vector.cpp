4f64cb6fc93c4eb9d5623a8af4a321aeb4e15f68
Vishal Raghav <vishalraghav321@gmail.com>
2025-12-24 15:57:14 +0530

    Fix vector initialization in vector.cpp
    Changed the initialization of the 2D vector 'vec' to avoid pre-sizing with 'n'. This may address issues with dynamic resizing or input handling.

--- experimental/vector.cpp
+++ experimental/vector.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     int n, q,l,j,a,k;
     cin >> n >> q;
-    vector<vector<int>> vec(n);
+    vector<vector<int>> vec;
      for(int i =0; i<n; i++){
         cin>>a;
          for(int j =0; j<a; j++){
